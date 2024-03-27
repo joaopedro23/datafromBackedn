606f5d4f6f674e7f06861538e8be9253d7ad77a3
joaopedro23 <joaopedro105manairapb@gmail.com>
2024-03-27 12:27:08 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ import strategy from "./routes/emaiLogin.routes";
 const app = express();
 
 app.use(cookieParser());
-app.use(cors(options));
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 app.use(authRoutes)
